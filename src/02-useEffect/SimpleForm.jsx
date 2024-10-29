c4350bf6eeba56ea8934d885b8ad5d0859292a69
Alexis Patiño <alexisivvi@outlook.com>
2024-10-28 21:57:12 -0700

    122 class starting with useEffect

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    userName: "alexis",
+    email: "alexis@hola",
+  });
+  const { userName, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    // const { name, value } = target;
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    console.log("userName", formState);
+  }, [formState]);
+  return (
+    <>
+      <h1>Simple form</h1>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="User Name"
+        name="userName"
+        value={userName}
+        onChange={onInputChange}
+      />
+      <input
+        type="email"
+        className="form-control tw-mt-4"
+        placeholder="alexis@email.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { StrictMode } from "react";
-import { CounterApp } from "./01-useState/CounterApp.jsx";
+// import { CounterApp } from "./01-useState/CounterApp.jsx";
+import { SimpleForm } from "./02-useEffect/SimpleForm.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/*<HooksApp />*/}
-    <CounterApp />
+    {/*<CounterApp />*/}
+    <SimpleForm />
   </StrictMode>,
 );
